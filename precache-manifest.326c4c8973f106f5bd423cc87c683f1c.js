08a88b01ba2a612b334ccf5fdd33e25b13280316
Ablutie <mishu_navetistul@yahoo.com>
2019-11-12 10:38:58 +0200

    Updates

---
+++ precache-manifest.326c4c8973f106f5bd423cc87c683f1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74256ff3ff8f4b49b582219110a10ad",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "b8e4c2fe97975748a985",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fc98fa883cd9b45cbe3c",
+    "url": "/robo-friends/static/css/main.fca8f1bf.chunk.css"
+  },
+  {
+    "revision": "b8e4c2fe97975748a985",
+    "url": "/robo-friends/static/js/2.e212711d.chunk.js"
+  },
+  {
+    "revision": "fc98fa883cd9b45cbe3c",
+    "url": "/robo-friends/static/js/main.a8ad0530.chunk.js"
+  },
+  {
+    "revision": "1214986d6de28420359d",
+    "url": "/robo-friends/static/js/runtime-main.191d373d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
