a095a53ab8d94aa0ed1f64c82e93dd1daca96137
Ablutie <mishu_navetistul@yahoo.com>
2019-11-12 10:31:15 +0200

    added error handing

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,8 @@ import SearchBox from "../components/SearchBox";
 import { robots } from "../components/robots";
 import Scroll from "../components/Scroll";
 import "./App.css";
+import ErrorBoundary from "../components/ErrorBoundary";
+
 
 class App extends Component {
     constructor() {
@@ -36,7 +38,9 @@ class App extends Component {
                 <h1 className="f1" >Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList robots={filterRobots} />
+                    <ErrorBoundary>
+                        <CardList robots={filterRobots} />
+                    </ErrorBoundary>
                 </Scroll>
             </div>
         );
